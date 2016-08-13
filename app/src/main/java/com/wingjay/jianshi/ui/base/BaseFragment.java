0ce8519653aa8afcce5d76f4abf78b00220d23bf
wingjay <wingjaysjtu@gmail.com>
2016-08-13 17:04:23 +0800

    create server repo

--- app/src/main/java/com/wingjay/jianshi/ui/base/BaseFragment.java
+++
@@ -1,9 +0,0 @@
-package com.wingjay.jianshi.ui.base;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by wingjay on 10/6/15.
- */
-public class BaseFragment extends Fragment {
-}
---
+++ server/README.md
@@ -0,0 +1,1 @@
+This repo is for JianShi-Server side. Based on Python and built on LeanCloud.
