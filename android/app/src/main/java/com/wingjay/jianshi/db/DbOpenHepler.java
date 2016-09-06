f8bc725ef702c5594580955f3dd4ee8d7d2bddf3
wingjay <wingjaysjtu@gmail.com>
2016-09-06 23:28:52 +0800

    [android] modify local db

--- android/app/src/main/java/com/wingjay/jianshi/db/DbOpenHepler.java
+++ android/app/src/main/java/com/wingjay/jianshi/db/DbOpenHepler.java
@@ -9,10 +9,10 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DbOpenHepler extends SQLiteOpenHelper {
 
-    public final static int DB_VAERION = 2;
+    public final static int DB_VAERION = 3;
     public final static String DB_NAME = "jianshi";
 
-    private final String CREATE_TABLE_DIARY_1 = "create table if not exists diary " +
+    private static final String CREATE_TABLE_DIARY_1 = "create table if not exists diary " +
             "(id integer primary key autoincrement," +
             "device_id string not null," +
             "title string not null," +
@@ -20,8 +20,19 @@ public class DbOpenHepler extends SQLiteOpenHelper {
             "created_time long not null," +
             "modified_time long )";
 
-    private final String ADD_COLUMN_DELETE_IN_DIARY_2 = "alter table diary " +
-            "add column deleted integer default 0";
+    private static final String ADD_COLUMN_DELETE_IN_DIARY_2 = "alter table diary " +
+            "add column deleted integer default 0 after content";
+
+    private static final String ADD_DIARY_ID_IN_DIARY_3 = "alter table diary" +
+        " add column diary_id integer after id; " +
+        " alter table diary add column time_removed long after modified_time";
+
+    private static final String[] sqlArray = new String[3];
+    static {
+        sqlArray[0] = CREATE_TABLE_DIARY_1;
+        sqlArray[1] = ADD_COLUMN_DELETE_IN_DIARY_2;
+        sqlArray[2] = ADD_DIARY_ID_IN_DIARY_3;
+    }
 
     public DbOpenHepler(Context context, String name, int version) {
         super(context, name, null, version);
@@ -35,12 +46,13 @@ public class DbOpenHepler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TABLE_DIARY_1);
         db.execSQL(ADD_COLUMN_DELETE_IN_DIARY_2);
+        db.execSQL(ADD_DIARY_ID_IN_DIARY_3);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        if (newVersion == 2) {
-            db.execSQL(ADD_COLUMN_DELETE_IN_DIARY_2);
+        for (int i=1; i<=newVersion; i++) {
+            db.execSQL(sqlArray[i-1]);
         }
     }
 }
