7c8137a34211b46db36517326d814bf1354cb7e6
wingjay <wingjaysjtu@gmail.com>
2016-10-26 21:06:31 +0800

    [android] fix inject bug

--- android/app/src/main/java/com/wingjay/jianshi/db/service/DiaryService.java
+++ android/app/src/main/java/com/wingjay/jianshi/db/service/DiaryService.java
@@ -27,10 +27,10 @@ public class DiaryService {
   private Context context;
 
   @Inject
-  private Gson gson;
+  Gson gson;
 
   @Inject
-  DiaryService(@ForApplication Context context) {
+  public DiaryService(@ForApplication Context context) {
     this.context = context;
   }
 
