68ea5a5506294c23c61d6cee599abfb48f8a4fa8
wingjay <wingjaysjtu@gmail.com>
2016-11-01 20:43:54 +0800

    tweak

--- server/server/data/home_images.py
+++ server/server/data/home_images.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 image_poem_list = [
     {
         'image': "http://difang.kaiwind.com/zhejiang/dfmssy/201311/12/W020131112347764553495.jpg",
+        'poem': '第一行-第二行-第三行',
     }
 ]
