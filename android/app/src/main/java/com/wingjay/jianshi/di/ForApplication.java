038db12c7d410c5771d8360365335b0d3efd087d
wingjay <wingjaysjtu@gmail.com>
2016-11-16 15:33:12 +0800

    [copyright] add copyright in android code

--- android/app/src/main/java/com/wingjay/jianshi/di/ForApplication.java
+++ android/app/src/main/java/com/wingjay/jianshi/di/ForApplication.java
@@ -1,3 +1,13 @@
+/*
+ * Created by wingjay on 11/16/16 3:31 PM
+ * Copyright (c) 2016.  All rights reserved.
+ *
+ * Last modified 8/27/16 11:07 PM
+ *
+ * Reach me: https://github.com/wingjay
+ * Email: yinjiesh@126.com
+ */
+
 package com.wingjay.jianshi.di;
 
 import java.lang.annotation.Retention;
--- android/app/src/main/java/com/wingjay/jianshi/sync/SyncService.java
+++ android/app/src/main/java/com/wingjay/jianshi/sync/SyncService.java
@@ -1,3 +1,13 @@
+/*
+ * Created by wingjay on 11/16/16 3:31 PM
+ * Copyright (c) 2016.  All rights reserved.
+ *
+ * Last modified 11/10/16 11:05 AM
+ *
+ * Reach me: https://github.com/wingjay
+ * Email: yinjiesh@126.com
+ */
+
 package com.wingjay.jianshi.sync;
 
 import android.app.IntentService;
