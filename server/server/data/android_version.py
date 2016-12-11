2c6f47a2e5aeef94b405a9d6aad2ddbbf7dabe7d
wingjay <wingjaysjtu@gmail.com>
2016-12-12 01:22:00 +0800

    new version in server to notify user

--- server/server/data/android_version.py
+++ server/server/data/android_version.py
@@ -2,12 +2,17 @@
 # -*- coding: utf-8 -*-
 
 newest_version = {
-    'version_name': '2.0.0',
-    'desc': '2.0正式版: 注册登录, 数据云端同步, 截屏分享和其它有意思的小功能',
+    'version_name': '2.1.0',
+    'desc': '不仅界面更加美美哒',
     'link': 'http://fir.im/vd1r'
 }
 
 version_history = [
+    {
+        'version_name': '2.0.0',
+        'desc': '2.0正式版: 注册登录, 数据云端同步, 截屏分享和其它有意思的小功能',
+        'link': 'http://fir.im/vd1r'
+    },
     {
         'version_name': '1.9.4',
         'desc': '2.0预览版',
